edba9cf98a37b912b22a33c42b118b8762ff1ccf
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-09-21 09:03:40 +0530

    Update ApiError.js

--- ApiError.js
+++ ApiError.js
@@ -1,1 +1,15 @@
 
+class ApiError extends Error {
+  constructor(statusCode, message = "Something went wrong", errors = []) {
+    super(message);
+
+    this.success = false;
+    this.statusCode = statusCode;
+    this.message = message;
+    this.errors = errors;
+    this.timestamp = new Date().toLocaleString('en-IN');
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export default ApiError;
