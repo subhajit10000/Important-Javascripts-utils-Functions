03f1dedf86be313da8a661383ea9aeb6d04d6948
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-09-22 08:50:45 +0530

    Update ApiResponse.js

--- ApiResponse.js
+++ ApiResponse.js
@@ -1,1 +1,13 @@
 
+class ApiResponse {
+  constructor(statusCode, message = "Success", data = null) {
+    this.success = statusCode < 400;
+    this.statusCode = statusCode;
+    this.message = message;
+    this.data = data;
+
+    this.timestamp = new Date().toLocaleString('en-IN');
+  }
+}
+
+export default ApiResponse;
