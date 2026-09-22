3b3b2e09aa624b0887f6c653c74405e5e5f0c900
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-09-22 08:50:59 +0530

    Enhance timestamp formatting in ApiResponse
    Updated timestamp format to include timezone and detailed date-time components.

--- ApiResponse.js
+++ ApiResponse.js
@@ -6,7 +6,16 @@ class ApiResponse {
     this.message = message;
     this.data = data;
 
-    this.timestamp = new Date().toLocaleString('en-IN');
+    this.timestamp = new Date().toLocaleString('en-IN', {
+    timeZone: 'Asia/Kolkata',
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: true 
+  });
   }
 }
 
