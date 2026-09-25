3e4769fca61b9741c8b25e07d40a04df0122a459
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-09-25 08:58:55 +0530

    Update generateOTP.js

--- generateOTP.js
+++ generateOTP.js
@@ -1,1 +1,16 @@
 
+import crypto from "crypto";
+import env from "../config/env.js";
+
+const generateOTP = (length = Number(env.OTP_LENGTH) || 6) => {
+    const digit = "0123456789";
+    let otp = "";
+    while (otp.length < length) {
+        const randomIndex = crypto.randomInt(0, digit.length);
+        otp += digit[randomIndex];
+    }
+
+    return otp;
+}
+
+export default generateOTP;
